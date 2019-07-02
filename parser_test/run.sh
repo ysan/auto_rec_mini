b1db6a52d6f432767630e1b2e30bddf1479ad3f0
kurihara <kurihara0-lj@infoseek.jp>
2019-07-02 09:18:30 +0900

    forget to commit

---
+++ parser_test/run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#BASE_DIR=${HOME}/prog/atpp/ts_parser_test
+BASE_DIR=./
+
+
+export LD_LIBRARY_PATH=\
+${BASE_DIR}/../ts_parser/aribstr:\
+${BASE_DIR}/../ts_parser/psisi:\
+${BASE_DIR}/../ts_parser/dsmcc:\
+${BASE_DIR}/../ts_parser:\
+${BASE_DIR}/../common:\
+
+
+
+if [ $# -ne 1 ]; then
+	echo "specify 1 argument."
+	exit 1
+fi
+
+if [ ! -e $1 -o ! -f $1 ]; then
+	echo "not existed [$1]"
+	exit 1
+fi
+
+${BASE_DIR}/tsparser_test -f $1
+#gdb ${BASE_DIR}/tsparser_test
+
