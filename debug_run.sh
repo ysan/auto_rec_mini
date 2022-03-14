4aaa474461327aef2bacf262698d7b1fd3bb20b5
yoshi@raspberry <kurihara0-lj@infoseek.jp>
2022-03-15 00:05:12 +0900

    add - debug run script

---
+++ debug_run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# build command
+#   make DEBUG_BUILD=1
+#   make DEBUG_BUILD=1 install
+
+
+# *** run this script with sudo ***
+
+ulimit -c unlimited
+
+export LD_LIBRARY_PATH=./local_build/lib:
+
+./local_build/bin/auto_rec_mini -c /opt/auto_rec_mini/settings.json -d /opt/auto_rec_mini
+
+
+# core file analysis after reproduction
+#   gdb ./local_build/bin/auto_rec_mini core
+#     gdb > bt
