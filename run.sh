649f9095f163b320b593c500294fe5be33057f17
yoshi@vs.sakura <kurihara0-lj@infoseek.jp>
2022-09-15 16:52:36 +0900

    update - Some improvements.

--- run.sh
+++ run.sh
@@ -4,5 +4,9 @@ ulimit -c unlimited
 
 export LD_LIBRARY_PATH=./local_build/lib:
 
-#	./local_build/bin/auto_rec_mini --conf=./settings.json
+if [ -f /opt/auto_rec_mini/settings.json ]; then
+	echo "run in /opt/auto_rec_mini..."
 	./local_build/bin/auto_rec_mini -c /opt/auto_rec_mini/settings.json -d /opt/auto_rec_mini
+else
+	./local_build/bin/auto_rec_mini --conf=./settings.json
+fi
