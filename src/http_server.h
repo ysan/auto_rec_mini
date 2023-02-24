47d0eb4a0b940f61fa2a22517856aa3f3b0b428a
Kurihara <kurihara0-lj@infoseek.jp>
2023-02-24 17:36:05 +0900

    Added - APIs (for viewing and channel)

--- src/http_server.h
+++ src/http_server.h
@@ -8,6 +8,7 @@
 #include "Utils.h"
 #include "Settings.h"
 #include "ViewingManagerIf.h"
+#include "ChannelManagerIf.h"
 
 
 class http_server {
@@ -17,7 +18,6 @@ public:
 	virtual ~http_server (void) = default;
 
 	void up (void) {
-
 		{
 			m_server.Post("/api/ctrl/viewing/start_by_physical_channel", [&](const httplib::Request &req, httplib::Response &res) {
 				if (req.get_header_value("Content-Type") != "application/json") {
@@ -57,6 +57,51 @@ public:
 				}
 				mp_ext_if->destroy_external_cp();
 			});
+
+			m_server.Post("/api/ctrl/viewing/start_by_service_id", [&](const httplib::Request &req, httplib::Response &res) {
+				if (req.get_header_value("Content-Type") != "application/json") {
+					res.status = 400;
+					return ;
+				}
+				if (req.body.empty()) {
+					res.status = 400;
+					return ;
+				}
+				const auto r = nlohmann::json::parse(req.body);
+				if (!r.contains("transport_stream_id")) {
+					res.status = 400;
+					return ;
+				}
+				if (!r.contains("original_network_id")) {
+					res.status = 400;
+					return ;
+				}
+				if (!r.contains("service_id")) {
+					res.status = 400;
+					return ;
+				}
+				uint16_t ts_id = r["transport_stream_id"].get<uint16_t>();
+				uint16_t org_nid = r["original_network_id"].get<uint16_t>();
+				uint16_t svc_id = r["service_id"].get<uint16_t>();
+
+				mp_ext_if->create_external_cp();
+				CViewingManagerIf::service_id_param_t param = {ts_id, org_nid, svc_id};
+				CViewingManagerIf _if (mp_ext_if);
+				_if.request_start_viewing_by_service_id(&param, false);
+				threadmgr::CSource &src = mp_ext_if->receive_external();
+				threadmgr::result rslt = src.get_result();
+				if (rslt == threadmgr::result::success) {
+					uint8_t gr = *(reinterpret_cast<uint8_t*>(src.get_message().data()));
+					nlohmann::json j;
+					j["group_id"] = static_cast<int>(gr);
+					res.set_content(j.dump(), "application/json");
+					res.set_header("Access-Control-Allow-Origin", req.get_header_value("Origin").c_str());
+				} else {
+					res.status = 500;
+				}
+				mp_ext_if->destroy_external_cp();
+			});
+
 			m_server.Post("/api/ctrl/viewing/stop", [&](const httplib::Request &req, httplib::Response &res) {
 				if (req.get_header_value("Content-Type") != "application/json") {
 					res.status = 400;
@@ -85,7 +130,126 @@ public:
 				}
 				mp_ext_if->destroy_external_cp();
 			});
+
+			m_server.Get("/api/ctrl/channel/channels", [&](const httplib::Request &req, httplib::Response &res) {
+				mp_ext_if->create_external_cp();
+				CChannelManagerIf::channel_t channels[20] = {0};
+				CChannelManagerIf::request_channels_param_t param = {channels, 20};
+				CChannelManagerIf _if (mp_ext_if);
+				_if.request_get_channels_sync(&param);
+				threadmgr::CSource &src = mp_ext_if->receive_external();
+				threadmgr::result rslt = src.get_result();
+				if (rslt == threadmgr::result::success) {
+					nlohmann::json j = nlohmann::json::array();
+					res.set_content(j.dump(), "application/json");
+					int ch_num = *(reinterpret_cast<int*>(src.get_message().data()));
+					if (ch_num > 0) {
+						for (int i = 0; i < ch_num; ++ i) {
+							nlohmann::json j_child;
+							j_child["physical_channel"] = channels[i].physical_channel;
+							j_child["transport_stream_id"] = channels[i].transport_stream_id;
+							j_child["original_network_id"] = channels[i].original_network_id;
+							j_child["remote_control_key_id"] = channels[i].remote_control_key_id;
+							j_child["service_ids"] = nlohmann::json::array();
+							for (int j = 0; j < channels[i].service_num; ++ j) {
+								j_child["service_ids"].push_back(channels[i].service_ids[j]);
+							}
+							j.push_back(j_child);
+						}
+					}
+					res.set_content(j.dump(), "application/json");
+					res.set_header("Access-Control-Allow-Origin", req.get_header_value("Origin").c_str());
+				} else {
+					res.status = 500;
+				}
+				mp_ext_if->destroy_external_cp();
+			});
+
+			m_server.Get(R"(/api/ctrl/channel/original_network_name/(\d+))", [&](const httplib::Request &req, httplib::Response &res) {
+				auto _m_org_nid = req.matches[1];
+
+				uint16_t org_nid = std::atoi(_m_org_nid.str().c_str());
+
+				mp_ext_if->create_external_cp();
+				CChannelManagerIf::service_id_param_t param = {0, org_nid, 0};
+				CChannelManagerIf _if (mp_ext_if);
+				_if.request_get_original_network_name(&param);
+				threadmgr::CSource &src = mp_ext_if->receive_external();
+				threadmgr::result rslt = src.get_result();
+				if (rslt == threadmgr::result::success) {
+					if (src.get_message().length() > 0) {
+						nlohmann::json j;
+						j["original_network_name"] = reinterpret_cast<char*>(src.get_message().data());
+						res.set_content(j.dump(), "application/json");
+						res.set_header("Access-Control-Allow-Origin", req.get_header_value("Origin").c_str());
+					} else {
+						res.status = 500;
+					}
+				} else {
+					res.status = 500;
+				}
+				mp_ext_if->destroy_external_cp();
+			});
+
+			m_server.Get(R"(/api/ctrl/channel/trasport_stream_name/(\d+)/(\d+))", [&](const httplib::Request &req, httplib::Response &res) {
+				auto _m_org_nid = req.matches[1];
+				auto _m_ts_id = req.matches[2];
+
+				uint16_t org_nid = std::atoi(_m_org_nid.str().c_str());
+				uint16_t ts_id = std::atoi(_m_ts_id.str().c_str());
+
+				mp_ext_if->create_external_cp();
+				CChannelManagerIf::service_id_param_t param = {ts_id, org_nid, 0};
+				CChannelManagerIf _if (mp_ext_if);
+				_if.request_get_transport_stream_name_sync(&param);
+				threadmgr::CSource &src = mp_ext_if->receive_external();
+				threadmgr::result rslt = src.get_result();
+				if (rslt == threadmgr::result::success) {
+					if (src.get_message().length() > 0) {
+						nlohmann::json j;
+						j["transport_stream_name"] = reinterpret_cast<char*>(src.get_message().data());
+						res.set_content(j.dump(), "application/json");
+						res.set_header("Access-Control-Allow-Origin", req.get_header_value("Origin").c_str());
+					} else {
+						res.status = 500;
+					}
+				} else {
+					res.status = 500;
+				}
+				mp_ext_if->destroy_external_cp();
+			});
+
+			m_server.Get(R"(/api/ctrl/channel/service_name/(\d+)/(\d+)/(\d+))", [&](const httplib::Request &req, httplib::Response &res) {
+				auto _m_org_nid = req.matches[1];
+				auto _m_ts_id = req.matches[2];
+				auto _m_svc_id = req.matches[3];
+
+				uint16_t ts_id = std::atoi(_m_org_nid.str().c_str());
+				uint16_t org_nid = std::atoi(_m_ts_id.str().c_str());
+				uint16_t svc_id = std::atoi(_m_svc_id.str().c_str());
+
+				mp_ext_if->create_external_cp();
+				CChannelManagerIf::service_id_param_t param = {ts_id, org_nid, svc_id};
+				CChannelManagerIf _if (mp_ext_if);
+				_if.request_get_service_name_sync(&param);
+				threadmgr::CSource &src = mp_ext_if->receive_external();
+				threadmgr::result rslt = src.get_result();
+				if (rslt == threadmgr::result::success) {
+					if (src.get_message().length() > 0) {
+						nlohmann::json j;
+						j["service_name"] = reinterpret_cast<char*>(src.get_message().data());
+						res.set_content(j.dump(), "application/json");
+						res.set_header("Access-Control-Allow-Origin", req.get_header_value("Origin").c_str());
+					} else {
+						res.status = 500;
+					}
+				} else {
+					res.status = 500;
+				}
+				mp_ext_if->destroy_external_cp();
+			});
 		}
+
 		{
 			// for CORS (preflight request)
 			m_server.Options("/api/.+", [](const httplib::Request &req, httplib::Response &res) {
@@ -115,7 +279,9 @@ public:
 	}
 
 	void down (void) {
+		_UTL_LOG_I ("HTTP stop...");
 		m_server.stop();
+		_UTL_LOG_I ("HTTP stop completed");
 	}
 
 private:
